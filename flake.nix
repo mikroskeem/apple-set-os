0b08b572872b9908db51385db86d88ea8c597239
Mark Vainomaa <mikroskeem@gmail.com>
2023-01-15 18:41:00 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Zentria repository boilerplate";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+
+          ];
+        };
+      });
+}
